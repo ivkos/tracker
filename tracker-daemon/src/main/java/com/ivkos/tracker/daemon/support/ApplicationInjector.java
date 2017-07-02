a947a84300456a5b680d082a37dde8f29ec62710
Ivaylo Stoyanov <me@ivkos.com>
2017-07-02 18:23:40 +0300

    Add ApplicationInjector

---
+++ tracker-daemon/src/main/java/com/ivkos/tracker/daemon/support/ApplicationInjector.java
@@ -0,0 +1,38 @@
+package com.ivkos.tracker.daemon.support;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Stage;
+import com.ivkos.tracker.daemon.gps.GpsModule;
+import com.ivkos.tracker.daemon.support.logging.LoggingModule;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ApplicationInjector
+{
+   private static final Logger logger = LogManager.getLogger(ApplicationInjector.class);
+   private static final Injector injector = createInjector();
+
+   private static Injector createInjector()
+   {
+      logger.info("Creating injector...");
+
+      Injector injector = Guice.createInjector(
+            Stage.DEVELOPMENT, // TODO: Change to PRODUCTION upon release
+
+            new LoggingModule(),
+            new ApplicationConfigurationModule("application.properties"),
+            new GpsModule(),
+            new GsonModule()
+      );
+
+      logger.info("Injector created successfully");
+
+      return injector;
+   }
+
+   public static Injector getInjector()
+   {
+      return injector;
+   }
+}
