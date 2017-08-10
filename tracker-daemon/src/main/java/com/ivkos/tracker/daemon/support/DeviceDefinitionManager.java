3ac4a605368f93df06b5c8b27fb378b962c12db2
Ivaylo Stoyanov <me@ivkos.com>
2017-08-10 16:04:24 +0300

    Daemon: Deserialization fixes

--- tracker-daemon/src/main/java/com/ivkos/tracker/daemon/support/DeviceDefinitionManager.java
+++ tracker-daemon/src/main/java/com/ivkos/tracker/daemon/support/DeviceDefinitionManager.java
@@ -1,5 +1,6 @@
 package com.ivkos.tracker.daemon.support;
 
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import com.ivkos.tracker.core.models.device.Device;
@@ -8,12 +9,20 @@ import io.vertx.core.json.Json;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 
 public class DeviceDefinitionManager
 {
+   static {
+      JavaTimeModule javaTimeModule = new JavaTimeModule();
+
+      Arrays.asList(Json.mapper, Json.prettyMapper)
+            .forEach(mapper -> mapper.registerModule(javaTimeModule));
+   }
+
    private final Device device;
 
    @Inject
