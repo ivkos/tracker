55821791af391fa90097447147f06e06c86f55e8
Ivaylo Stoyanov <me@ivkos.com>
2017-08-23 17:38:20 +0300

    Update init script

--- tracker-daemon/scripts/init_device.sh
+++ tracker-daemon/scripts/init_device.sh
@@ -6,7 +6,6 @@ API_SECRET=THE_API_SECRET_GOES_HERE
 
 
 # Script
-set -e
 
 if [ "$API_BASE_URL" = "THE_API_BASE_URL_GOES_HERE" ]; then
     echo "Please edit the API_BASE_URL variable in this script before running"
@@ -25,11 +24,28 @@ fi
 
 DEVICE_ID=$((16#$(sed -n '/^Serial/{s/.* //;p}' /proc/cpuinfo)))
 
-mkdir -p /var/lib/tracker
-curl \
+TRACKER_USERNAME=tracker
+
+useradd -m -U ${TRACKER_USERNAME}
+chmod 775 /home/${TRACKER_USERNAME}
+
+sudo -u tracker \
+    curl \
     -f -s \
-    -o /var/lib/tracker/device.json \
+    -o /home/${TRACKER_USERNAME}/device.json \
     -H "X-Api-Secret: ${API_SECRET}" \
     ${API_BASE_URL}/devices/uuid-of/${DEVICE_ID}
+
+cat <<EOF > /etc/systemd/system/tracker.service
+[Unit]
+Description=GPS Tracker
+[Service]
+WorkingDirectory=/home/${TRACKER_USERNAME}
+ExecStart=/usr/bin/java -jar /home/${TRACKER_USERNAME}/tracker-daemon-1.0.0-jar-with-dependencies.jar
+Type=simple
+User=${TRACKER_USERNAME}
+[Install]
+WantedBy=multi-user.target
+EOF
 
 echo Success
