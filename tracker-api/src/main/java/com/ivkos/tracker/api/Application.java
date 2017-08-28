956fd5a4eb6a1eea248a680112f9608ee3638499
Ivaylo Stoyanov <me@ivkos.com>
2017-08-28 17:55:52 +0300

    Fix timestamps not persisted correctly

--- tracker-api/src/main/java/com/ivkos/tracker/api/Application.java
+++ tracker-api/src/main/java/com/ivkos/tracker/api/Application.java
@@ -4,9 +4,14 @@ import com.ivkos.tracker.core.models.ModelsPackageMarker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
-@EntityScan(basePackageClasses = { ModelsPackageMarker.class, Application.class })
+@EntityScan(basePackageClasses = {
+      ModelsPackageMarker.class,
+      Application.class,
+      Jsr310JpaConverters.class
+})
 public class Application
 {
    public static void main(String[] args)
