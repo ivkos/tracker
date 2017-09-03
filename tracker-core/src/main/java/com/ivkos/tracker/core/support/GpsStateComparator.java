96dd06ff374685dc4436a2f6ed64ae61c18108f6
Ivaylo Stoyanov <me@ivkos.com>
2017-09-03 18:57:46 +0300

    Move GpsStateComparator to core

---
+++ tracker-core/src/main/java/com/ivkos/tracker/core/support/GpsStateComparator.java
@@ -0,0 +1,40 @@
+package com.ivkos.tracker.core.support;
+
+import com.ivkos.tracker.core.models.gps.GpsState;
+
+import java.time.OffsetDateTime;
+import java.util.Comparator;
+
+public class GpsStateComparator implements Comparator<GpsState>
+{
+   private static GpsStateComparator instance;
+
+   @Override
+   public int compare(GpsState o1, GpsState o2)
+   {
+      OffsetDateTime state1SatTime = o1.getSatelliteTime();
+      OffsetDateTime state2SatTime = o2.getSatelliteTime();
+
+      if (state1SatTime != null && state2SatTime != null) {
+         return state1SatTime.compareTo(state2SatTime);
+      }
+
+      OffsetDateTime state1LastUpdatedTime = o1.getLastUpdatedTime();
+      OffsetDateTime state2LastUpdatedTime = o2.getLastUpdatedTime();
+
+      if (state1LastUpdatedTime != null && state2LastUpdatedTime != null) {
+         return state1LastUpdatedTime.compareTo(state2LastUpdatedTime);
+      }
+
+      return 0;
+   }
+
+   public static GpsStateComparator getInstance()
+   {
+      if (instance == null) {
+         instance = new GpsStateComparator();
+      }
+
+      return instance;
+   }
+}
--- tracker-daemon/src/main/java/com/ivkos/tracker/daemon/gps/GpsStateHistoryHolder.java
+++ tracker-daemon/src/main/java/com/ivkos/tracker/daemon/gps/GpsStateHistoryHolder.java
@@ -1,16 +1,15 @@
 package com.ivkos.tracker.daemon.gps;
 
 import com.ivkos.tracker.core.models.gps.GpsState;
+import com.ivkos.tracker.core.support.GpsStateComparator;
 
-import java.time.OffsetDateTime;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
 
 public class GpsStateHistoryHolder
 {
-   private final ConcurrentSkipListSet<GpsState> history = new ConcurrentSkipListSet<>(new GpsStateComparator());
+   private final ConcurrentSkipListSet<GpsState> history = new ConcurrentSkipListSet<>(GpsStateComparator.getInstance());
 
    public void add(GpsState state)
    {
@@ -25,28 +24,5 @@ public class GpsStateHistoryHolder
    public boolean removeAll(Set<GpsState> s)
    {
       return history.removeAll(s);
-   }
-
-   static class GpsStateComparator implements Comparator<GpsState>
-   {
-      @Override
-      public int compare(GpsState o1, GpsState o2)
-      {
-         OffsetDateTime state1SatTime = o1.getSatelliteTime();
-         OffsetDateTime state2SatTime = o2.getSatelliteTime();
-
-         if (state1SatTime != null && state2SatTime != null) {
-            return state1SatTime.compareTo(state2SatTime);
-         }
-
-         OffsetDateTime state1LastUpdatedTime = o1.getLastUpdatedTime();
-         OffsetDateTime state2LastUpdatedTime = o2.getLastUpdatedTime();
-
-         if (state1LastUpdatedTime != null && state2LastUpdatedTime != null) {
-            return state1LastUpdatedTime.compareTo(state2LastUpdatedTime);
-         }
-
-         return 0;
-      }
    }
 }
