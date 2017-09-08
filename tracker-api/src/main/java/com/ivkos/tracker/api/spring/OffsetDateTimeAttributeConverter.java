6aa396e7a523d25b046668f74b677398371d2bf0
Ivaylo Stoyanov <me@ivkos.com>
2017-09-09 00:11:35 +0300

    API: Persist OffsetDateTime as Timestamp

--- tracker-api/src/main/java/com/ivkos/tracker/api/spring/OffsetDateTimeAttributeConverter.java
+++ tracker-api/src/main/java/com/ivkos/tracker/api/spring/OffsetDateTimeAttributeConverter.java
@@ -2,39 +2,31 @@ package com.ivkos.tracker.api.spring;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
+import java.sql.Timestamp;
 import java.time.OffsetDateTime;
-import java.time.chrono.IsoChronology;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
-import java.time.format.ResolverStyle;
 
-import static java.time.temporal.ChronoField.*;
+import static java.time.ZoneOffset.UTC;
 
 @Converter(autoApply = true)
-public class OffsetDateTimeAttributeConverter implements AttributeConverter<OffsetDateTime, String>
+public class OffsetDateTimeAttributeConverter implements AttributeConverter<OffsetDateTime, Timestamp>
 {
-   private static final DateTimeFormatter formatter = new DateTimeFormatterBuilder()
-         .parseCaseInsensitive()
-         .appendValue(YEAR, 4)
-         .appendValue(MONTH_OF_YEAR, 2)
-         .appendValue(DAY_OF_MONTH, 2)
-         .appendValue(HOUR_OF_DAY, 2)
-         .appendValue(MINUTE_OF_HOUR, 2)
-         .appendValue(SECOND_OF_MINUTE, 2)
-         .appendOffset("+HHMM", "+0000")
-         .toFormatter()
-         .withResolverStyle(ResolverStyle.STRICT)
-         .withChronology(IsoChronology.INSTANCE);
-
    @Override
-   public String convertToDatabaseColumn(OffsetDateTime attribute)
+   public Timestamp convertToDatabaseColumn(OffsetDateTime dateTime)
    {
-      return attribute == null ? null : attribute.format(formatter);
+      if (dateTime == null) {
+         return null;
+      }
+
+      return Timestamp.from(dateTime.toInstant());
    }
 
    @Override
-   public OffsetDateTime convertToEntityAttribute(String dbData)
+   public OffsetDateTime convertToEntityAttribute(Timestamp timestamp)
    {
-      return dbData == null ? null : OffsetDateTime.parse(dbData, formatter);
+      if (timestamp == null) {
+         return null;
+      }
+
+      return OffsetDateTime.ofInstant(timestamp.toInstant(), UTC);
    }
 }
