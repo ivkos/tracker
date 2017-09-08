e686115646b226feb023e37a76bcbcc3871f8272
Ivaylo Stoyanov <me@ivkos.com>
2017-09-08 21:46:10 +0300

    Update persistence timestamp format for faster and more accurate sorting

--- tracker-api/src/main/java/com/ivkos/tracker/api/spring/OffsetDateTimeAttributeConverter.java
+++ tracker-api/src/main/java/com/ivkos/tracker/api/spring/OffsetDateTimeAttributeConverter.java
@@ -3,19 +3,34 @@ package com.ivkos.tracker.api.spring;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
+import static java.time.temporal.ChronoField.*;
 
 @Converter(autoApply = true)
 public class OffsetDateTimeAttributeConverter implements AttributeConverter<OffsetDateTime, String>
 {
+   private static final DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+         .parseCaseInsensitive()
+         .appendValue(YEAR, 4)
+         .appendValue(MONTH_OF_YEAR, 2)
+         .appendValue(DAY_OF_MONTH, 2)
+         .appendValue(HOUR_OF_DAY, 2)
+         .appendValue(MINUTE_OF_HOUR, 2)
+         .appendValue(SECOND_OF_MINUTE, 2)
+         .appendOffset("+HHMM", "+0000")
+         .toFormatter();
+
    @Override
    public String convertToDatabaseColumn(OffsetDateTime attribute)
    {
-      return attribute == null ? null : attribute.toString();
+      return attribute == null ? null : attribute.format(formatter);
    }
 
    @Override
    public OffsetDateTime convertToEntityAttribute(String dbData)
    {
-      return dbData == null ? null : OffsetDateTime.parse(dbData);
+      return dbData == null ? null : OffsetDateTime.parse(dbData, formatter);
    }
 }
